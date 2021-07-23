310132b9555c2413d4c9a96e0518f08085792e0a
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-07-23 20:07:29 +0100

    dangerous edit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
+import { Vue, createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import VueParticles from 'vue-particles'
 
 createApp(App).mount('#app')
+
+Vue.use(VueParticles)
