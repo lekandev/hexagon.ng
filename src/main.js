16fa1a4a118f5530545a016a47ed50716cdc1be9
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-07-26 12:31:01 +0100

    favicon and vue-meta

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-// import Vue from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 // import VueParticles from 'vue-particles'
