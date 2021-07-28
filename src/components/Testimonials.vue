50c98ac316de0a9cf5019cebf1f594c10eb5ed0c
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-07-28 02:38:14 +0100

    Update Testimonials.vue

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="testimonials">
-      <Testimonial content="Blacksmelt, tripod fish grunt mackerel vendace surfperch carpsucker surgeonfish streamer fish boga deepwater stingray. Spaghetti" image="team__member2.png" author="Lekandev" />
+      <Testimonial content="Blacksmelt, tripod fish grunt mackerel vendace surfperch carpsucker surgeonfish streamer fish boga deepwater stingray. Spaghetti Blacksmelt, tripod fish grunt mackerel vendace surfperch carpsucker surgeonfish streamer fish boga deepwater stingray. Spaghetti Blacksmelt, tripod fish grunt mackerel vendace surfperch carpsucker surgeonfish streamer fish boga deepwater stingray. Spaghetti Blacksmelt, tripod fish grunt mackerel vendace surfperch carpsucker surgeonfish streamer fish boga deepwater stingray. Spaghetti" image="team__member2.png" author="Lekandev" />
   </section>
 </template>
 
